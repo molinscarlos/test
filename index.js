9277004a8480fa5c3cef3142c9c1cc31ee96277d
Carlos Molins <carlos.molins@dribba.com>
2022-11-04 06:58:25 +0100

    change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const sentencesController = require('./controllers/controller.sentences');
 const viewController = require('./controllers/controller.view');
 const firebase = require('firebase-admin');
